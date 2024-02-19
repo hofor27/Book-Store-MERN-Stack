dc78b50c85628fb603c9968bb1fce45d65c985ef
NitramDom <Jordan.delaunay@yandex.com>
2024-02-19 19:50:04 +0500

    Package HSON File changed

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,7 @@
-import express from "express"
-import {PORT} from "./config.js"
+import express from "express";
+import { PORT } from "./config.js";
 const app = express();
-// middleware
 
 app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`);
-  });
-
+  console.log(`App is listening to port: ${PORT}`);
+});
