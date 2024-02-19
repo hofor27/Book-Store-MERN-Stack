26835bb12952b425472ed40ec28b2bb3f04b6c96
hofor27 <hofor27@gmail.com>
2024-02-19 12:21:05 -0300

    Frontend Cleaning

---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <div>
+        <button>Table</button>
+        <button>Card</button>
+        </div>
+        <div><h1>Books List</h1></div>
+    </div>
+  )
+}
+
+export default Home
