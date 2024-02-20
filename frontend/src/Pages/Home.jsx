02ab170e2aca6d988f14eeb6232c9d8d1c1476eb
NitramDom <Jordan.delaunay@yandex.com>
2024-02-20 19:29:07 +0500

    UI Updated

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-
 const Home = () => {
   return (
-    <div>
-        <div>
-        <button className='bg-red-400'>Table</button>
-        <button>Card</button>
-        </div>
-        <div><h1>Books List</h1></div>
+    <div className="p-4">
+      <div className="flex justify-center items-center gap-x-4">
+        <button className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg">
+          Table
+        </button>
+        <button className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg">
+          Card
+        </button>
+      </div>
+      <div className="flex justify-between items-center">
+        <h1 className="text-3xl my-8">Books List</h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
